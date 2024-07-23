4f0aa9bf6c3b2c42c323e3171a77a3caf228ca5f
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-07-23 22:01:07 +0300

    Add mobile navigation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
+
+  //Hamburger menu function
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  }
+
+  //Existing class names for the div
+  const baseClass = 'navbar__container';
+
   return (
     <nav className='navbar'>
       <div className='navbar__logo'>
       <Link to="home" spy={true} smooth={true} offset={-280} duration={800}> Lakmal. </Link>
       </div>
-      <div className="navbar__container">
-        <ul className='navbar__links'>
-            <li className='navbar__item'>
+      <div className={`${baseClass} ${isActive ? 'active' : ''}`}>
+        <ul>
+            <li>
               <Link className='navbar__link' to="home" spy={true} smooth={true} offset={-280} duration={800}> Home </Link>
             </li>
-            <li className='navbar__item'>
+            <li>
               <Link className='navbar__link' to="about" spy={true} smooth={true} offset={-150} duration={800}> About </Link>
             </li>
-            <li className='navbar__item'>
+            <li>
               <Link className='navbar__link' to="portfolio" spy={true} smooth={true} offset={-150} duration={800}> Portfolio </Link>
             </li>
-            <li className='navbar__item'>
+            <li>
             <Link className='navbar__link' to="contact" spy={true} smooth={true} offset={-150} duration={800}> Contact </Link>
             </li>
         </ul>
+      </div>
+      <div className="hamburger" onClick={handleClick}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
       </div>
     </nav>
   )
